2ab347f5bfacab99ee4ae3de7619785ba01587a3
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-01 01:00:50 +0930

    try deploying 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public' + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/public'))
