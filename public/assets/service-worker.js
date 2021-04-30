806e20d0824e1740c2679d5958549e3568eccb9a
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-04-30 16:36:20 +0930

    adding files to cache

--- public/assets/service-worker.js
+++ public/assets/service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
     '/assets/js/index.js',
     '/assets/css/style.css',
     '/dist/app.bundle.js',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
+    '/dist/db.bundle.js',
+    '/dist/serviceWorker.bundle.js',
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
   ];
